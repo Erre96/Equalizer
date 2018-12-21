a244307cb548ae5196ff4493f2947e69deecf656
Erhan <erre_erhan@hotmail.com>
2018-12-21 12:06:35 +0100

    Access audio files partly done

--- app/src/main/java/com/example/erhan/equalizer/MusicCollectionActivity.java
+++ app/src/main/java/com/example/erhan/equalizer/MusicCollectionActivity.java
@@ -1,40 +1,116 @@
 package com.example.erhan.equalizer;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MusicCollectionActivity extends AppCompatActivity {
-    private RecyclerView mRecyclerView;
+    final String MEDIA_PATH = Environment.getExternalStorageDirectory() .getPath() + "/";
+    private static final int MY_PERMISSION_REQUEST = 1;
+
+    private ListView listView;
+    ArrayAdapter adapter;
+    ArrayList arrayList;
+
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_collection);
-        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
+        //mRecyclerView.setHasFixedSize(true);
 
         // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
+        //mLayoutManager = new LinearLayoutManager(this);
+        //mRecyclerView.setLayoutManager(mLayoutManager);
 
-        String[] input = new String[10];
-        for (int i = 0; i < input.length; i++) {
-            input[i] = ("Linkin Park - In The End " + i);
-        }// define an adapter
-
-        // specify an adapter (see also next example)
-        mAdapter = new MyAdapter(input);
-        mRecyclerView.setAdapter(mAdapter);
+        if(ContextCompat.checkSelfPermission(MusicCollectionActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MusicCollectionActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                ActivityCompat.requestPermissions(MusicCollectionActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSION_REQUEST);
+            }
+        } else {
+            //doStuff();
+        }
     }
-    // ...
+
+    public void doStuff() {
+        listView = findViewById(R.id.my_recycler_view);
+        arrayList = new ArrayList<>();
+        getMusic();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+
+    public void getMusic(){
+        ContentResolver contentResolver = getContentResolver();
+        Uri songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        final Cursor songCursor = getContentResolver().query(null,null,null,null);
+
+        if(songCursor != null && songCursor.moveToFirst()) {
+            int songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            int songArtist = songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+            int songLocation = songCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
+
+            do {
+                String currentTitle = songCursor.getString(songTitle);
+                String currentArtist = songCursor.getString(songArtist);
+                String currentLocation = songCursor.getString(songLocation);
+
+                arrayList.add(currentTitle + "\n" + currentArtist + "\n"+"Artist : "+currentArtist + "\n"+"Location : "+currentLocation);
+            } while(songCursor.moveToNext());
+        }
+    }
+
+    @Override
+    public void OnRequestPermissionResult(int requstCode, String[] permissions, int[] grantResults) {
+        switch (requstCode) {
+            case MY_PERMISSION_REQUEST: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(MusicCollectionActivity.this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show();
+
+                        doStuff();
+                    }
+                } else {
+                    Toast.makeText(this, "No Permission granted!", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                return;
+            }
+        }
+    }
 }
